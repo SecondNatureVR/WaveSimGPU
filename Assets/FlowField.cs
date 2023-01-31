c203cdc899fc3d73c991310535b458960c07f47f
Johnny <secondnaturevr@gmail.com>
2023-01-31 00:24:00 -0500

    just init TRS in cpu

--- Assets/FlowField.cs
+++ Assets/FlowField.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -11,53 +12,63 @@ public class FlowField : MonoBehaviour
     private Bounds bounds;
     private Vector3Int dimensions;
     private int flowBufferSize;
-    private ComputeBuffer flowVectorBuffer;
-    private ComputeBuffer argsBuffer;
+    private ComputeBuffer flowVectors;
     private RenderParams renderParams;
+
+    struct FlowVector
+    {
+        // position is implied by indexing into buffer
+        public Matrix4x4 rotation;
+        public Matrix4x4 transform;
+        public float magnitude;
+    }
+    Vector3 GetPosition(int index) {
+        Vector3Int dims = Vector3Int.RoundToInt(bounds.size);
+        int z = index % dims.z;
+        int y = (index / dims.z) % dims.y;
+        int x = index / (dims.y * dims.z);
+        return bounds.min + new Vector3(x,y,z) + new Vector3(.5f, .5f, .5f);
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         bounds = GetComponent<Collider>().bounds;
         dimensions = Vector3Int.CeilToInt(bounds.size);
-        int stride = System.Runtime.InteropServices.Marshal.SizeOf(typeof(Vector3));
 
         // create buffers for vectors 
         flowBufferSize = dimensions.x * dimensions.y * dimensions.z;
 
-
-        stride = System.Runtime.InteropServices.Marshal.SizeOf(typeof(Matrix4x4));
-        flowVectorBuffer = new ComputeBuffer(flowBufferSize, stride);
-        Vector3[] positions = new Vector3[flowBufferSize];
+        int stride = System.Runtime.InteropServices.Marshal.SizeOf(typeof(FlowVector));
+        flowVectors = new ComputeBuffer(flowBufferSize, stride);
 
         // Compute transformation matrices
         // https://forum.unity.com/threads/rotate-mesh-inside-shader.1109660/
-        Matrix4x4[] vectors = new Matrix4x4[flowBufferSize];
+        FlowVector[] vectors = new FlowVector[flowBufferSize];
         int index = 0;
-        for (int x = 0; x < dimensions.x; x++)
+        for (int i = 0; i < flowBufferSize; i++)
         {
-            for (int y = 0; y < dimensions.y; y++)
+            Vector3 pos = GetPosition(i);
+            Vector3 flow = bounds.center - pos;
+            if (pos.z < bounds.center.z)
             {
-                for (int z = 0; z < dimensions.z; z++)
-                {
-                    Vector3 pos = bounds.min + new Vector3(x, y, z) + new Vector3(0.5f, 0.5f, 0.5f);
-                    positions[index] = pos;
-                    Vector3 flow = pos;
-                    float scaling = flow.magnitude / bounds.extents.magnitude;
-                    if (z > bounds.extents.z)
-                    {
-                        flow = bounds.center - pos;
-                        scaling = 1 - scaling;
-                    }
-                    Quaternion rot = Quaternion.FromToRotation(Vector3.up, flow.normalized);
-                    vectors[index] = Matrix4x4.TRS(pos, rot, Vector3.one * Mathf.Pow(scaling, 2));
-                    index++;
-                }
+                flow = pos - bounds.center;
             }
+            Quaternion rot = Quaternion.FromToRotation(Vector3.up, flow.normalized);
+            FlowVector v = new FlowVector();
+            v.rotation = Matrix4x4.Rotate(rot);
+            v.transform = Matrix4x4.TRS(pos, rot, Vector3.one);
+            v.magnitude = Mathf.InverseLerp(1, bounds.extents.magnitude, flow.magnitude);
+            vectors[index] = v;
+            index++;
         }
-        flowVectorBuffer.SetData(vectors);
+        flowVectors.SetData(vectors);
 
-        instancedMaterial.SetBuffer("flowVectorBuffer", flowVectorBuffer);
+        instancedMaterial.SetBuffer("flowVectors", flowVectors);
+        instancedMaterial.SetVector("BOUNDS_MIN", bounds.min);
+        instancedMaterial.SetInt("WIDTH", dimensions.x);
+        instancedMaterial.SetInt("HEIGHT", dimensions.y);
+        instancedMaterial.SetInt("DEPTH", dimensions.z);
 
         renderParams = new RenderParams(instancedMaterial);
         renderParams.worldBounds = bounds;
@@ -71,12 +82,12 @@ public class FlowField : MonoBehaviour
 
     private void OnDisable()
     {
-        flowVectorBuffer.Release();
-        flowVectorBuffer = null;
+        flowVectors.Release();
+        flowVectors = null;
     }
 
     private void OnDestroy()
     {
-        flowVectorBuffer.Release();
+        flowVectors.Release();
     }
 }
--- Assets/FlowVector.shader
+++ Assets/FlowVector.shader
@@ -24,30 +24,56 @@ Shader "Instanced/FlowVector"
 
         sampler2D _MainTex;
 
+        struct FlowVector {
+            float4x4 rotation;
+            float4x4 transform;
+            float magnitude;
+        };
+
         struct Input
         {
             float2 uv_MainTex;
         };
 
         fixed4 _ColorMin, _ColorMax;
+        float3 BOUNDS_MIN;
+        uint WIDTH, HEIGHT, DEPTH;
 
 		#ifdef SHADER_API_D3D11
-			StructuredBuffer<float3> positionBuffer;
-			StructuredBuffer<float4x4> flowVectorBuffer;
+			StructuredBuffer<FlowVector> flowVectors;
 		#endif
 
-        void setup() {
+        // https://stackoverflow.com/questions/14845084/how-do-i-convert-a-1d-index-into-a-3d-index
+        float3 GetPosition(uint index) {
+			int z = index % DEPTH;
+			int y = (index / DEPTH) % HEIGHT;
+			int x = index / (HEIGHT * DEPTH);
+            return BOUNDS_MIN + float3(x,y,z) + float3(.5, .5, .5);
+        }
 
+        void setup() {
 		#ifdef UNITY_PROCEDURAL_INSTANCING_ENABLED
 			#ifdef SHADER_API_D3D11
-				float3 position     = positionBuffer[unity_InstanceID];
-				float4x4 flowvec	    = flowVectorBuffer[unity_InstanceID];
-			#else
-				float3 position = 0;
-				float4x4 flowvec = 0;
-			#endif
+				FlowVector flow = flowVectors[unity_InstanceID];
 
-              unity_ObjectToWorld = flowvec;
+                // Translate matrix
+                float3 pos = GetPosition(unity_InstanceID);
+                float4x4 translate = 0.0;
+                translate._m00_m11_m22_m33 = float4(1,1,1,1);
+                translate._m03_m13_m23 = pos;
+
+                // Rotation matrix
+                float4x4 rotation = flow.rotation;
+
+                // Scale matrix
+                float s = pow(1.0 - flow.magnitude, 2);
+                float4x4 scale = 0.0;
+                scale._m00_m11_m22_m33 = float4(s,s,s,1.0);
+
+                // Compose transform matrix
+                //unity_ObjectToWorld = mul(translate, scale);
+                unity_ObjectToWorld = mul(flow.transform, scale);
+            #endif
         #endif
         }
 
@@ -55,14 +81,12 @@ Shader "Instanced/FlowVector"
         {
 			#ifdef UNITY_PROCEDURAL_INSTANCING_ENABLED
                  // TODO: extract scaling from TRS matrix
-                float3 pos = flowVectorBuffer[unity_InstanceID]._m03_m13_m23;
-				//float strength = length(s); 
-                float strength = length(pos) / 10;
+                float strength = flowVectors[unity_InstanceID].magnitude;
             #else
                 float strength = 0.5f;
 			#endif
-            o.Albedo = lerp(_ColorMin, _ColorMax, 1 - strength);
-            o.Alpha = 0.01f;
+            o.Albedo = lerp(_ColorMin, _ColorMax, strength);
+            o.Alpha = 0.1f;
         }
         ENDCG
     }
